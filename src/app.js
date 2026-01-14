5d9b0409eae7f62b688ab2bce6db6a2dad8c95de
Nattha <gamenattha555@gmail.com>
2026-01-14 10:55:12 +0700

    user-route

--- src/app.js
+++ src/app.js
@@ -1,36 +1,36 @@
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import helmet from "helmet";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import helmet from "helmet";
 
 import { router as apiRoutes } from "./routes/index.js";
-import { limiter } from "./middlewares/rateLimiter.js";
+// import { limiter } from "./middlewares/rateLimiter.js";
 
 export const app = express();
 
 app.set("trust proxy", 1);
 
 // Global middleware
-app.use(helmet());
+// app.use(helmet());
 
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "https://jsd-react-assessment-solution.vercel.app",
-  ],
-  credentials: true, // ✅  allow cookies to be sent
-};
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "http://localhost:5174",
+//     "http://localhost:5175",
+//     "https://jsd-react-assessment-solution.vercel.app",
+//   ],
+//   credentials: true, // ✅  allow cookies to be sent
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-app.use(limiter);
+// app.use(limiter);
 
 app.use(express.json());
 
 // Middleware to parse cookies (required for cookie-based auth)
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
