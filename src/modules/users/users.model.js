779f2c5b0d33887ff73a09aca7419f93f0e44225
Nattha <gamenattha555@gmail.com>
2026-01-19 13:13:11 +0700

    user schema

--- src/modules/users/users.model.js
+++ src/modules/users/users.model.js
@@ -1,11 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, trim: true    },
-    role: {type: String, enum:["user","admin"], default:"user"},
-    email: {type: String, required: true, trim: true, unique: true, lowercase: true},
-    password: {type: String, required: true, minlength: 6},
+const userSchema = new mongoose.Schema(
+  {
+    userName: { type: String, required: true, trim: true },
+    userLast: { type: String, required: true, trim: true },
+    img:{type: String},
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true, minlength: 6, select: false },
+    phoneNumber: { type: String, minlength: 10, maxlength: 10 },
+    address: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-})
-
-export const TeaUser = mongoose.model("User",userSchema)
+export const TeaUser = mongoose.model("User", userSchema);
