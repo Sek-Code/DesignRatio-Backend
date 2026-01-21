d8e1497c49f6a16b42f7d377ea9dc2a3d2c58d50
Jakkrit Saychang <56269911+boatlittlebear@users.noreply.github.com>
2026-01-21 10:23:38 +0700

    create orders api

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { router as usersRoutes } from "./users.routes.js";
-import { router as productRoutes} from "./products.routes.js"
+import { router as productRoutes } from "./products.routes.js";
+import ordersRoutes from "./orders.routes.js";
+
 export const router = Router();
 
 router.use("/users", usersRoutes);
-
-router.use("/products", productRoutes)
+router.use("/products", productRoutes);
+router.use("/orders", ordersRoutes);
